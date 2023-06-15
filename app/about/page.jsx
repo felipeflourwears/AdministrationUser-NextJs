efbe6746d0ac63bf2b64a12fc2bc2215d93195a0
felipeflourwears <A01274880@tec.mx>
2023-06-14 23:02:17 -0600

    First Commit

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, reiciendis nostrum? Et ducimus odio itaque magni quod eum eligendi vitae aut voluptatum, vero fugiat, aperiam, impedit quisquam corrupti nihil pariatur.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
