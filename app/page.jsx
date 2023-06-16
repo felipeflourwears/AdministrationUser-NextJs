93c4d0b0e4a0a0da30b631c9eb3a38f30431d15f
felipeflourwears <A01274880@tec.mx>
2023-06-15 19:34:10 -0600

    Working pages Users

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,23 @@
+import Users from '@/components/users'
 
-function IndexPage(){
+async function fetchUsers() {
+  const res = await fetch("https://reqres.in/api/users");
+  const data = await res.json();
+  console.log(data.data); //Imprimir data del json
+  console.log("Hello");
+  return data.data;
+}
+
+async function IndexPage() {
+  const users = await fetchUsers();
+  console.log(users)
   return(
     <div>
-      <h1>Hello world</h1>
+      <h1>Index Page</h1>
+      {/* {JSON.stringify(users)} */}
+      <Users users={users}/>
     </div>
-  )
+  );
 }
-export default IndexPage
+
+export default IndexPage;
---
+++ app/users/[id]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UsersPage() {
+  return (
+    <div>
+      <h1>User Details</h1>
+      
+    </div>
+  )
+}
+
+export default UsersPage
